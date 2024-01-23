ebc36ed6ac100f60a1316ab9cd723ac957549f16
GecineiCristofolini <gecinei.c@hotmail.com>
2024-01-23 07:46:12 -0300

    About e DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,43 +1,72 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
-    @FXML
+	@FXML
 	private MenuItem menuItemVendedor;
-    @FXML
+	@FXML
 	private MenuItem menuItemDespartamento;
-    @FXML
+	@FXML
 	private MenuItem menuItemSobre;
-	
-    @FXML
-    public void onMenuItemVendedorAction () {
-    	
-    	System.out.println("Ação Menu Item Vendedor");
-    }
-    
-    @FXML
-    public void onMenuItemDepartamentoAction () {
-    	
-    	System.out.println("Ação Menu Item Departamento");
-    }
-    
-    @FXML
-    public void onMenuItemSobreAction () {
-    	
-    	System.out.println("Ação Menu Item Sobre");
-    }
-    
-    
+
+	@FXML
+	public void onMenuItemVendedorAction() {
+
+		System.out.println("Ação Menu Item Vendedor");
+	}
+
+	@FXML
+	public void onMenuItemDepartamentoAction() {
+
+		loadView("/gui/DepartmentList.fxml");;
+	}
+
+	@FXML
+	public void onMenuItemSobreAction() {
+
+		loadView("/gui/About.fxml");;
+	}
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
+
+	}
+
+	private synchronized void loadView(String absoluteName) {
 		
+		try {
+			   FXMLLoader 	loader = new FXMLLoader(getClass().getResource(absoluteName));
+			   VBox newVBox = loader.load();
+			   
+			   Scene mainScene = Main.getMainScene();
+			   VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			   
+			   Node mainMenu = mainVBox.getChildren().get(0);
+			   mainVBox.getChildren().clear();
+			   mainVBox.getChildren().add(mainMenu);
+			   mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Expetion", "Error Load Page", e.getMessage(), AlertType.ERROR);
+				
+			}
 	}
 
 }
